dd30a68b6129b07389bb3737a0df2c6bea276790
WinterCT <160752384+WinterCT@users.noreply.github.com>
2024-03-02 23:58:13 +0100

    Add files via upload

---
+++ CyberTankz/index.php
@@ -0,0 +1,195 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>CyberTankz</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="assets/images/favicon.png">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/icofont.min.css">
+	<link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+	<link rel="stylesheet" href="assets/css/lightcase.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+	<div class="preloader">
+		<div class="preloader-inner">
+			<div class="preloader-icon">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	</div>
+
+	<header class="header-section">
+		<div class="container">
+			<div class="header-holder">
+				<div class="header-menu-part">
+					<div class="header-bottom d-flex flex-wrap justify-content-between align-items-center">
+						<div class="brand-logo d-none d-lg-inline-block">
+							<div class="logo">
+								<a href="index.html">
+									<img src="assets/images/logo/logo.png" alt="logo">
+								</a>
+							</div>
+						</div>
+						<div class="header-wrapper justify-content-lg-end">
+							<div class="mobile-logo d-lg-none">
+								<a href="index.html"><img src="assets/images/logo/logo.png" alt="logo"></a>
+							</div>
+							<div class="menu-area">
+								<ul class="menu">
+								    <li><a href="">Home</a></li>
+									<li><a href="news">News</a></li>
+									<li><a href="">eSports</a></li>
+								    <li><a href="">Forum</a></li>
+                                    <li><a href="">Wiki</a></li>
+									<li>
+										<a href="javascript:void(0);">Others</a>
+										<ul class="submenu">
+											<li><a href="materials">Materials</a></li>
+											<li><a href="rules">Rules</a></li>
+											<li><a href="programs">Game Programs</a></li>
+										</ul>
+									</li>
+								</ul>
+
+								<div class="header-bar d-lg-none">
+									<span></span>
+									<span></span>
+									<span></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</header>
+
+	<section class="banner" style="background-image: url(assets/images/banner/bg-2.jpg);">
+		<div class="container">
+			<div class="row g-0">
+				<div class="col-xxl-6 col-lg-7 col-md-9 col-sm-10 col-12">
+					<div class="banner__content">
+						<h1>CyberTankz</h1>
+						<p>It's time to embark on a journey across the battlefield where the past meets the present. The game will 
+take you from 2020 to 2024 and offer you an unforgettable experience. With over 31 ranks to achieve,
+each map offers familiar yet new battles and challenges. Plenty of special quests and unique rewards 
+await you to make you stand out on the battlefield. The future of CyberTankz will be developed based 
+on your feedback, but rest assured that both new and old fan-favourite features are in store. Join us as 
+we  embark on a "rewritten past" with CyberTankz!</p>
+						<a href="" class="default-btn"><span>Download</span> </a>
+						<a href="" class="default-btn"><span>Discord</span> </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div class="blog-section padding-top padding-bottom">
+		<div class="container">
+			<div class="section-header">
+				<h2>Latest News</h2>
+			</div>
+			<div class="section-wrapper">
+				<div class="row g-4 justify-content-center row-cols-lg-2 row-cols-1">
+				<div class="col">
+						<div class="blog-item">
+							<div class="blog-inner">
+								<div class="blog-thumb">
+									<img src="assets/images/news/01.jpg" alt="blog-thumb">
+								</div>
+								<div class="blog-content px-3 py-4">
+									<a href="">
+										<h3>Helpers Recruitment</h3>
+									</a>
+									<div class="meta-post">
+										<a href="#">CyberTankz</a>
+										<a href="#">05 March 2024</a>
+									</div>
+									<p>Join our team as a helper and have a real impact on our community! We are looking for people who are ready to support us in various areas. We offer a vast selection of positions so there is able to find his own job based on his passion.</p>
+									<a href="" class="default-btn reverse-effect"><span>More Informations</span></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="blog-item">
+							<div class="blog-inner">
+								<div class="blog-thumb">
+									<img src="assets/images/news/02.jpg" alt="blog-thumb">
+								</div>
+								<div class="blog-content px-3 py-4">
+									<a href="">
+										<h3>Invite your friend to our Discord server</h3>
+									</a>
+									<div class="meta-post">
+										<a href="#">CyberTankz</a>
+										<a href="#">19 February 2024</a>
+									</div>
+									<p>We are pleased to invite you to participate in our first contest, the goal of 
+										which is to invite your friends. This way, we aim to increase the number of players
+										 on the discord server. Nobody knows when the game will be released!<br>Good Luck!</p>
+									<a href="" class="default-btn reverse-effect"><span>More Informations</span></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer class="footer-section">
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+					    <div class="footer-bottom-content text-center">
+							<p>©2024. All Rights Reserved by <b>CyberTankz</b></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+	<script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+	<script src="assets/js/circularProgressBar.min.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/swiper-bundle.min.js"></script>
+	<script src="assets/js/lightcase.js"></script>
+	<script src="assets/js/countdown.min.js"></script>
+	<script src="assets/js/waypoints.min.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+	<script src="assets/js/plugins.js"></script>
+	<script src="assets/js/main.js"></script>
+
+
+	<script>
+		window.ga = function () {
+			ga.q.push(arguments)
+		};
+		ga.q = [];
+		ga.l = +new Date;
+		ga('create', 'UA-XXXXX-Y', 'auto');
+		ga('set', 'anonymizeIp', true);
+		ga('set', 'transport', 'beacon');
+		ga('send', 'pageview')
+	</script>
+	<script src="https://www.google-analytics.com/analytics.js" async></script>
+
+
+
+</body>
+
+</html>
